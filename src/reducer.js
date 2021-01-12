1f9a018ab6a0cd4c17cfbc9e16ebc78b157d587d
Dv <dv@Dvs-MacBook-Pro.local>
2021-01-12 20:55:04 +0530

    cart

---
+++ src/reducer.js
@@ -0,0 +1,35 @@
+const reducer = (state, action) => {
+  if (action.type === "CLEAR_CART") {
+    return { ...state, cart: [] };
+  }
+  if (action.type === "REMOVE_ITEM") {
+    return {
+      ...state,
+      cart: state.cart.filter((item) => item.id !== action.payload),
+    };
+  }
+  if (action.type === "INCREASE") {
+    let tempCart = state.cart.map((item) => {
+      if (item.id === action.payload) {
+        return { ...item, amount: item.amount + 1 };
+      }
+      return item;
+    });
+    return { ...state, cart: tempCart };
+  }
+  if (action.type === "DECREASE") {
+    let tempCart = state.cart
+      .map((item) => {
+        if (item.id === action.payload) {
+          return { ...item, amount: item.amount - 1 };
+        }
+        return item;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+    return { ...state, cart: tempCart };
+  }
+
+  return state;
+};
+
+export default reducer;
